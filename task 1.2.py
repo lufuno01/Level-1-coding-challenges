071f5b044bae21da0465c5822a1c429566b3c36f
lufuno01 <104906398+lufuno01@users.noreply.github.com>
2022-05-27 02:10:34 +0200

    task 1.2

--- task 1.2.py
+++ task 1.2.py
@@ -1,6 +1,9 @@
-def calculate_sum(num1, num2):
- if num1 == num2 or (num1 + num2 ==3):
-  print("True")
- else:
-  print("False")
-calculate_sum(1,12)
+def test_number3(num1, num2):
+    if num1 == num2 or (num1 + num2 == 3):
+        print("True")
+
+    else:
+        print("False")
+
+
+test_number3(1, 12)
